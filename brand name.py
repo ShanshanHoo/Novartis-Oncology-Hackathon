b96d43da2b6f30d137ef568e6ff97d36cb375360
ShanshanHoo <hushan0910@gmail.com>
2020-10-06 23:09:37 -0400

    1006 1109pm

---
+++ brand name.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 21:58:51 2020
+
+@author: Shanshan Hu
+"""
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn as sk
+import re
+
+os.chdir("/Users/conta/Documents/Novartis")
+
+dg = pd.read_csv('drug_pxrx.csv', header=0)
+
+def label_brand (row):
+   if row['drug_generic_name'] == 'PALBOCICLIB' or row['drug_name'] == 'IBRANCE':
+      return 'IBR'
+   if row['drug_generic_name'] == 'FULVESTRANT' or row['drug_name'] == 'FASLODEX':
+      return 'FAS'
+   if row['drug_generic_name'] == 'RIBOCICLIB' or row['drug_name'] == 'KISQALI' or row['drug_name'] == 'KISQALI FEMARA' or row['drug_generic_name'] == 'RIBOCICLIB SUCCINATE-LETROZOLE':
+      return 'KIS'
+   if row['drug_generic_name'] == 'EVEROLIMUS' or row['drug_name'] == 'AFINITOR':
+      return 'AFI'
+   if row['drug_generic_name'] == 'LETROZOLE' or row['drug_name'] == 'FEMARA':
+      return 'LET'
+   if row['drug_generic_name'] == 'ABEMACICLIB' or row['drug_name'] == 'VERZENIO':
+      return 'VER'
+   if row['drug_generic_name'] == 'CHEMO' or row['drug_name'] == 'CHEMO':
+      return 'CHEMO'
+   if row['drug_generic_name'] == 'CAPECITABINE' or row['drug_name'] == 'XELODA':
+      return 'XEL'
+   if row['drug_name'] == 'TAMOXIFEN CITRATE':
+      return 'TAM'
+   if row['drug_generic_name'] == 'ANASTROZOLE' or row['drug_generic_name'] == 'EXEMESTANE' or row['drug_name'] == 'ANASTROZOLE' or row['drug_name'] == 'ARIMIDEX' or row['drug_name'] == 'EXEMESTANE' or row['drug_name'] == 'AROMASIN' or row['drug_name'] == 'NOLVADEX' or row['drug_name'] == 'SOLTAMOX':
+      return 'AI'
+   return 'OTHERS'
+dg['brand'] = dg.apply (lambda row: label_brand(row), axis=1)
+dg.to_csv("brand.csv", index = False)
---
+++ merge drug.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 20:41:13 2020
+
+@author: Shanshan Hu
+"""
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn as sk
+import re
+
+os.chdir("/Users/conta/Documents/Novartis")
+
+## merge rx_px_combine with drug reference
+prx = pd.read_csv('rx_px_combine.csv', header=0)
+
+dg_rf = pd.read_csv('Drug Reference.csv',header=0)
+dg_rf = dg_rf[dg_rf.columns[0:3]] 
+dg_rf.head()
+dg=pd.merge(prx,dg_rf,how='left',on='drug_id',validate="m:1")
+
+# check intersection, check merge accuracy
+len(dg['drug_id'].unique()) # 7806
+len(dg_rf['drug_id'].unique()) # 532725
+len(dg_rf) # 532725
+len(prx['drug_id'].unique()) # 7806
+len(prx) # 8930837
+df2 = pd.DataFrame(dg_rf['drug_id'].unique())
+df3 = pd.DataFrame(prx['drug_id'].unique())
+intersected_df = pd.merge(df3, df2, how='inner')
+len(intersected_df) # 21
+
+df2 = pd.DataFrame(dg_rf['drug_id'])
+df3 = pd.DataFrame(prx['drug_id'])
+intersected_df = pd.merge(df3, df2, how='inner')
+len(intersected_df) # 429451
+len(intersected_df['drug_id'].unique()) # 21
+
+dg.to_csv("drug_pxrx.csv", index = False)
+
+#px = pd.read_csv('PX.txt', 
+#                   dtype={"PATIENT_ID": int,"CLAIM_ID": object,"CLAIM_LINE_ITEM": int,
+#                          "CLAIM_TYP_CD": object,"PROCEDURE_CODE": object,"PRC1_MOD_CD": object,"PRC1_MOD_DESC": object,
+#                          "PRC_VERS_TYP_ID": object,"PROVIDER_BILLING_ID": int,"PROVIDER_FACILITY_ID": int,
+#                          "PROVIDER_REFERRING_ID": int,"PROVIDER_RENDERING_ID": int,"SVC_CRGD_AMT": float,
+#                          "SERVICE_DATE": str,"MONTH_ID": int,"UNIT_OF_SVC_AMT": float,
+#                          "PLACE_OF_SERVICE": object,"PAYER_PLAN_ID": object,"PAY_TYPE": object,"NDC": object,
+#                          "PRODUCT": object,"DIAGNOSIS_CODE": object,"DIAG_CD_POSN_NBR": int,"DIAG_VERS_TYP_ID": int,
+#                          "DIAG_DESC": object,"WEEK_END_FRI": object,
+#                          "RESTATE_FLAG": object,"FLEXIBLE_FLD_1_CHAR": object,"FLEXIBLE_FLD_2_CHAR": object}, 
+#                   sep="|")
+#
+#dg_rf = pd.read_csv('Drug Reference.csv',header=0)
+#dg_rf = dg_rf[dg_rf.columns[0:3]] 
+#dg_rf.head()
+#
+#px.rename(columns={'PROCEDURE_CODE':'drug_id'}, inplace=True)
+#px.columns
+#dg=pd.merge(px,dg_rf,how='outer',on='drug_id',validate="m:1")
+#
+## check intersect, check merge accuracy
+#df2 = pd.DataFrame(dg_rf['drug_id'].unique())
+#df3 = pd.DataFrame(px['drug_id'].unique())
+#intersected_df = pd.merge(df3, df2, how='inner')
+#len(intersected_df) # 20
+#
+#df2 = pd.DataFrame(dg_rf['drug_id'])
+#df3 = pd.DataFrame(px['drug_id'])
+#intersected_df = pd.merge(df3, df2, how='inner')
+#len(intersected_df) # 475688
+#len(px) # 9297565
+#len(dg_rf) # 532725
+#len(intersected_df['drug_id'].unique()) # 20
+
+
+
+
